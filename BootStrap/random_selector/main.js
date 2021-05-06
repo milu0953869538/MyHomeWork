7edb1b8604c4440ab56301138501d4de99175925
milu0953869538 <83447858+milu0953869538@users.noreply.github.com>
2021-05-06 16:20:21 +0800

    Add files via upload

---
+++ BootStrap/random_selector/main.js
@@ -0,0 +1,8 @@
+   $(function(){
+      $("input").on("click",function(){
+         var numberOfListItem = $("#choices li").length;
+         var randomChildNumber = Math.floor(Math.random()*numberOfListItem)
+         $("h1").text($("#choices li").eq(randomChildNumber).text());
+        // $("img").attr("src",$("li").eq(randomChildNumber).attr("title"));
+      });
+   });
